669d0a882f752b1d010049a46908404009266d16
Matthew <macrossan17@gmail.com>
2022-04-29 00:07:01 -0700

    add hash helper function

---
+++ helpers/hash-password.py
@@ -0,0 +1,23 @@
+import bcrypt
+
+
+def generateHash(password):
+    hashed = bcrypt.hashpw(password.encode('ASCII'), bcrypt.gensalt())
+    return hashed
+
+
+def validateHash(password, hashPassword):
+    b_password = password.encode('ASCII')
+    if bcrypt.checkpw(b_password, hashPassword):
+        print('correct')
+    else:
+        print('wrong')
+
+
+res = generateHash('helloworld')
+print(validateHash('helloworld', res))
+
+# password -> hash algo -> new hashed password
+# one way
+# hashpw -
+# gensalt - generates a random string to make password hard to hack
