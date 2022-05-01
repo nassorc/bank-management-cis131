2d7927cd7a2691b851ca033eb82156eb83adcae7
Matthew <macrossan17@gmail.com>
2022-05-01 16:06:25 -0700

    create database file

---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlite3
+import pandas as pd
+
+
+class DATABASE:
+    def __init__(self):
+        self.FIRSTNAME = ""
+        self.LASTNAME = ""
+        self.EMAIL = ""
+        try:
+            self.connection = sqlite3.connect('accounts.db')
+        except Exception as e:
+            print("Error: Database class could not establish connection with database.")
+
+    def query(self, query):
+        return pd.read_sql(query, self.connection)
+
+    def add_account(self, first, last, email, password):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(
+                f"INSERT INTO accounts (email, password, first_name, last_name) VALUES('{email}', '{password.decode('ASCII')}', '{first}', '{last}');")
+        except Exception as e:
+            print(f"Error: addAccount did not create new user\n--{e}")
+            return False
+
+        self.connection.commit()
+        return True
+
+    def findByEmail(self, email):
+        return pd.read_sql(f"SELECT * FROM accounts WHERE email='{email}'", self.connection)
+
+    def closeDatabase(self):
+        self.connection.close()
+
+
+# db = DATABASE()
+# print(db.query('SELECT * FROM accounts'))
--- main.py
+++ main.py
@@ -3,42 +3,7 @@ from tkinter import messagebox
 import sqlite3
 import pandas as pd
 import helpers.hash_password as bcrypt
-
-
-class DATABASE:
-    def __init__(self):
-        self.FIRSTNAME = ""
-        self.LASTNAME = ""
-        self.EMAIL = ""
-        try:
-            self.connection = sqlite3.connect('accounts.db')
-        except Exception as e:
-            print("Error: Database class could not establish connection with database.")
-
-    def query(self, query):
-        return pd.read_sql(query, self.connection)
-
-    def add_account(self, first, last, email, password):
-        try:
-            cursor = self.connection.cursor()
-            cursor.execute(
-                f"INSERT INTO accounts (email, password, first_name, last_name) VALUES('{email}', '{password.decode('ASCII')}', '{first}', '{last}');")
-        except Exception as e:
-            print(f"Error: addAccount did not create new user\n--{e}")
-            return False
-
-        self.connection.commit()
-        return True
-
-    def findByEmail(self, email):
-        return pd.read_sql(f"SELECT * FROM accounts WHERE email='{email}'", self.connection)
-
-    def closeDatabase(self):
-        self.connection.close()
-
-
-# db = DATABASE()
-# print(db.query('SELECT * FROM accounts'))
+import database
 
 
 class Login:
@@ -80,7 +45,7 @@ class Login:
             msg.grid(row=6, column=0)
             return
         try:
-            db = DATABASE()
+            db = database.DATABASE()
             user = db.findByEmail(email)
             user = user.to_dict()
 
@@ -145,7 +110,7 @@ class Login:
 
         hashed = bcrypt.generateHash(password)
         print(len(hashed))
-        db = DATABASE()
+        db = database.DATABASE()
         res = db.add_account(first, last, email, hashed)
 
         if res:
@@ -158,8 +123,11 @@ class Login:
         self.login.mainloop()
 
 
-# login_page = Login()
-# login_page.mainloop_window()
+login_page = Login()
+db = database.DATABASE()
+print(db.query('SELECT * FROM accounts'))
+
+login_page.mainloop_window()
 
 
 class BANK_MANAGEMENT:
