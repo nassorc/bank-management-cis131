af0719f97fb6b7479bf70d4ebb374dacdaa71686
Matthew <macrossan17@gmail.com>
2022-05-02 22:08:08 -0700

    update root ui

--- main.py
+++ main.py
@@ -7,36 +7,44 @@ import database
 
 
 class Login:
-    def __init__(self, login=Tk()):
-        self.login = login
+    def __init__(self, login_window=Tk()):
+        self.login_window = login_window
         # this prevent opening the main window when clicking close without
         # logging in
-        login.protocol("WM_DELETE_WINDOW", self.close)
-        login.geometry("340x300")
-        login.title("Login")
+        login_window.protocol("WM_DELETE_WINDOW", self.close)
+        login_window.geometry("410x300")
+        login_window.title("Login")
         # if user logs in return id
         # variable gives information about the user to the main window
         self.user_id = 0
 
-        self.login_email_label = Label(login, text="email")
-        self.login_password_label = Label(login, text="password")
-        self.user_email = Entry(login, width=35)
-        self.user_password = Entry(login, width=35, show="*")
-        self.login_btn = Button(login, text="Login", width=35,
+        self.login_frame = Frame(
+            login_window, width=340, height=300, padx=20)
+
+        self.login_logo = Label(
+            self.login_frame, text="Bank Manager", font=48, pady=15)
+        self.login_email_label = Label(self.login_frame, text="email")
+        self.login_password_label = Label(self.login_frame, text="password")
+        self.user_email = Entry(self.login_frame, width=35)
+        self.user_password = Entry(self.login_frame, width=35, show="*")
+        self.login_btn = Button(self.login_frame, text="Login", width=35,
                                 command=self.handle_login)
-        self.register_btn = Button(login, text="open account", width=35,
+        self.register_btn = Button(self.login_frame, text="open account", width=35,
                                    command=self.register_ui)
 
         # text fields
         # email_text = Text(root, )
 
         # add to window
+        self.login_logo.grid(row=0, column=2)
         self.login_email_label.grid(row=1, column=2, sticky=W)
         self.login_password_label.grid(row=2, column=2, sticky=W)
         self.user_email.grid(row=1, column=3, columnspan=5, sticky=E+W)
         self.user_password.grid(row=2, column=3, columnspan=5, sticky=E+W)
         self.login_btn.grid(row=3, column=3, columnspan=5, sticky=E+W)
         self.register_btn.grid(row=4, column=3, columnspan=5)
+
+        self.login_frame.pack()
 
     def close(self):
         exit()
@@ -46,9 +54,11 @@ class Login:
         password = self.user_password.get()
 
         if not email or not password:
-            msg = Label(
-                self.login, text="Please include an email address and password")
-            msg.grid(row=6, column=0)
+            # msg = Label(
+            #     self.login_window, text="Please include an email address and password")
+            # msg.place()
+            messagebox.showwarning(
+                title="incomplete", message='Please include an email and password')
             return
         try:
             db = database.DATABASE()
@@ -58,7 +68,7 @@ class Login:
             if bcrypt.validateHash(password, user['password'][0]):
                 print('user logged in')
                 self.user_id = user['id'][0]
-                self.login.destroy()  # removes this window
+                self.login_window.destroy()  # removes this window
                 return
             else:
                 messagebox.showwarning(
@@ -73,25 +83,25 @@ class Login:
         return
 
     def register_ui(self):
-        win = Toplevel(self.login)
-        self.win = win
-        win.title("Register")
+        register_window = Toplevel(self.login_window)
+        self.register_window = register_window
+        register_window.title("Register")
 
         # Input fields
-        self.first_entry = Entry(win, width=32)
-        self.last_entry = Entry(win, width=32)
-        self.email_entry = Entry(win, width=32)
-        self.password_entry = Entry(win, width=32)
+        self.first_entry = Entry(register_window, width=32)
+        self.last_entry = Entry(register_window, width=32)
+        self.email_entry = Entry(register_window, width=32)
+        self.password_entry = Entry(register_window, width=32)
 
         # labels
-        self.first_label = Label(win, text="First name")
-        self.last_label = Label(win, text="Last name")
-        self.email_label = Label(win, text="email")
-        self.password_label = Label(win, text="password")
+        self.first_label = Label(register_window, text="First name")
+        self.last_label = Label(register_window, text="Last name")
+        self.email_label = Label(register_window, text="email")
+        self.password_label = Label(register_window, text="password")
 
         # button
         self.register_btn = Button(
-            win, text="create", command=self.create_account)
+            register_window, text="create", command=self.create_account)
 
         # place on register window
         self.first_label.grid(row=0, column=0, sticky='w')
@@ -121,17 +131,16 @@ class Login:
 
         if res:
             messagebox.showinfo(title="User created", message="user created")
-            self.win.destroy()
+            self.register_window.destroy()
         else:
             messagebox.showwarning(title="Error", message="server error")
 
     def mainloop_window(self):
-        self.login.mainloop()
+        self.login_window.mainloop()
 
 
 login_page = Login()
 db = database.DATABASE()
-print(db.query('SELECT * FROM accounts'))
 
 login_page.mainloop_window()
 
@@ -142,21 +151,46 @@ class BANK_MANAGEMENT:
         root.title("Bank management")
         root.geometry('600x400')
 
+        # get user information
         self.user_id = user_id
         db = database.DATABASE()
-        self.user_information = db.findById(self.user_id).to_dict
-        print(f'MAIN WINDOW {self.user_information}')
-        frame1 = Frame(root, width=520, height=200, highlightbackground='red',
-                       highlightthickness=3)
-        frame1.grid(row=0, column=0, padx=20, pady=10, ipadx=20)
+        self.user = db.findById(self.user_id).to_dict()
 
-        label = Label(frame1, text="window1", font=(16))
-        label.grid(row=0, column=0)
+        self.frame1 = LabelFrame(root, width=520, height=100, padx=10, pady=10)
+        self.frame1.grid(row=0, column=0, columnspan=2,
+                         padx=20, pady=10, ipadx=20, sticky=E+W)
+
+        self.logo_label = Label(self.frame1, text="Bank Management", font=(48))
+        self.logo_label.place(relx=.04, rely=.35)
+
+        self.user_email_label = Label(
+            self.frame1, text=f"email: {self.user['email'][0]}")
+        self.user_email_label.place(relx=.70, rely=.15)
+
+        self.balance_label = Label(
+            self.frame1, text=f"Balance: ${self.user['balance'][0]}")
+        self.balance_label.place(relx=.70, rely=.45)
+
+        self.frame2 = Frame(root, width=520, height=60)
+        self.frame2.grid(row=1, columnspan=2,
+                         padx=20, pady=10, sticky=E+W)
+
+        self.deposit_btn = Button(self.frame2, text="deposit", width=18)
+        self.deposit_btn.grid(row=0, column=0, padx=3)
+
+        self.withdraw_btn = Button(self.frame2, text="withdraw", width=18)
+        self.withdraw_btn.grid(row=0, column=1)
+
+        self.transfer_btn = Button(self.frame2, text="transfer", width=18)
+        self.transfer_btn.grid(row=0, column=2)
+
+        self.activity_btn = Button(self.frame2, text="activity", width=18)
+        self.activity_btn.grid(row=0, column=3)
 
     def mainloop_root(self):
         self.root.mainloop()
 
 
+# main_win = BANK_MANAGEMENT(5)
 main_win = BANK_MANAGEMENT(login_page.user_id)
 main_win.mainloop_root()
-# main_win.mainloop_root(login_page.user_id)
